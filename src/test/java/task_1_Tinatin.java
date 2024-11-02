fa2a94947f5f090ff4c5c59a721434848248aa96
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-11-02 22:57:27 +0400

    task_1_Tinatin

---
+++ src/test/java/task_1_Tinatin.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class task_1_Tinatin {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            System.out.println("Enter the first number:");
+            int ricxvi1 = scanner.nextInt();
+            System.out.println("Enter the second number:");
+            int ricxvi2 = scanner.nextInt();
+
+            int ganayofi = ricxvi1 / ricxvi2;
+            System.out.println("The division is: " + ganayofi);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } finally {
+            scanner.close();
+        }
+    }
+}
